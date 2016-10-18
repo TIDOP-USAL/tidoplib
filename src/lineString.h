5e3c8e2feaaf0b0a6de35a3a2faf2b3d48d43362
Rocío Mora <rociomora@usal.es>
2016-10-18 13:56:29 +0200

    Añadida la clase LineString.cpp con su cabecera LineString.h y su documento asociado de pruebas. Editor: Rocío Mora. Fecha: 18-Oct_2016

--- src/lineString.h
+++ src/lineString.h
@@ -4,8 +4,8 @@
 #include "opencv2/core/core.hpp"
 #include "utils.h"
 #include "geometric_entities\window.h"
-
-class I3D_EXPORT lineString 
+#include <cstdarg>
+class I3D_EXPORT LineString 
 {
 public:
 	/*Una LineString estará formada por una colección de puntos
@@ -14,17 +14,17 @@ public:
 	std::vector<cv::Point> vertices;
 public:
 	/*constructor por defecto -> Crea un vector con 0 elementos*/
-	lineString();
+	LineString();
 
 	/*constructor de copia*/
-	lineString(const lineString &linestring);
+	LineString(const LineString &linestring);
 
 	/*constructor a partir de una colección de puntos
 	Se pasan los puntos como una funcion con numero indeterminado de args*/
-	lineString(cv::Point p1, ...);
+	LineString(std::initializer_list<cv::Point> listPoints);
 
 	/*destructor*/
-	~lineString();
+	~LineString();
 public: 
 	/*Metodo para saber el tamaño del vector*/
 	int getSize() { return vertices.size(); };
@@ -32,5 +32,14 @@ public:
 	/*Metodo para obtener el vertice con indice deseado*/
 	cv::Point getPoint(int indice) { return vertices[indice];}
 };
+
+/*CONSTRUCTOR POR DEFECTO. Crea la clase y el vector de vértices con 0 elementos*/
+LineString::LineString() : vertices(0) {}
+
+/*CONSTRUCTOR DE COPIA. Crea una nueva clase a partir de una ya existente. Copia el vector*/
+LineString::LineString(const LineString &linea) : vertices(linea.vertices) {}
+
+/*CONSTRUCTOR A PARTIR DE LISTA DE PUNTOS- Con una colección de puntos creamos la polilinea. */
+LineString::LineString(std::initializer_list<cv::Point> listPoints): vertices(listPoints) {}
 
 #endif
