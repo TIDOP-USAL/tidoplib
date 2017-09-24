7d4c98af7737158e02ea74be7995df10e0d891c4
Esteban <estebanrdo@gmail.com>
2017-09-24 19:17:13 +0200

    Se cambia el estado en el batch al terminar. No se estaba haciendo y aparecia como RUNNING las siguientes veces que se intentaba acceder.

--- src/img_process/filters.cpp
+++ src/img_process/filters.cpp
@@ -248,6 +248,7 @@ ImgProcessing::Status Sobel::execute(const cv::Mat &matIn, cv::Mat *matOut) cons
   cv::Mat abs_grad_x, abs_grad_y;
   try {
     cv::Sobel(matIn, grad_x, mDepth, mDx, mDy, mKernelSize, mScale, mDelta, mBorderType);
+    //TODO: No tiene mucho sentido añadir esto dentro del filtro Sobel
     convertScaleAbs(grad_x, abs_grad_x);
     threshold(abs_grad_x, *matOut, mThresh, mMaxVal, cv::THRESH_BINARY);
   } catch (cv::Exception &e){
