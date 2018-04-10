15f8240e8a794c6b686a771a7be728fd0d5b0f67
estebanrdo <estebanrdo@gmail.com>
2018-04-10 14:01:07 +0200

    - Se elimina del repositorio los directorios build, install y googletest que se han subido por error. - CMakeList.txt:   - Nivel de warning cambiado de 4 a 3.   - Establecidas las directivas del precompilador: _SCL_SECURE_NO_WARNINGS y _CRT_SECURE_NO_DEPRECATE

--- build/googletest-build/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Desarrollo/TidopLib/build/googletest-src/googletest
-# Build directory: C:/Desarrollo/TidopLib/build/googletest-build/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
