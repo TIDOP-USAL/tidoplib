d79cf76ec49312676b80d433844728037fa809a6
esteban <estebanrdo@gmail.com>
2020-09-03 18:13:29 +0200

    Programa crstransform para convertir entre sistemas de referencia

--- apps/crstrf/crstransform.cpp
+++ apps/crstrf/crstransform.cpp
@@ -27,22 +27,21 @@ int main(int argc, char** argv)
 
   std::string epsg_in;
   std::string epsg_out;
-  double coord_x = 0.0;
-  double coord_y = 0.0;
-  double coord_z = 0.0;
+  std::string coord;
+  std::string separator = ";";
+  std::string coord_trf;
 
-  // Se definen los parámetros y opciones
   Command cmd(cmd_name, "Ejemplo de transformación de coordenadas");
   cmd.push_back(std::make_shared<ArgumentStringRequired>("epsg_in", 'i', "Sistema de referencia de entrada", &epsg_in));
   cmd.push_back(std::make_shared<ArgumentStringRequired>("epsg_out", 'o', "Sistema de referencia de salida", &epsg_out));
-  cmd.push_back(std::make_shared<ArgumentDoubleRequired>("coord_x", 'x', "Coordenadas X", &coord_x));
-  cmd.push_back(std::make_shared<ArgumentDoubleRequired>("coord_y", 'y', "Coordenadas Y", &coord_y));
-  cmd.push_back(std::make_shared<ArgumentDoubleOptional>("coord_z", 'z', "Coordenada Z", &coord_z));
+  cmd.push_back(std::make_shared<ArgumentStringRequired>("coord", 'c', "Fichero de texto con las coordenadas separadas por comas o cadena de texto con las coordenadas de un punto", &coord));
+  cmd.push_back(std::make_shared<ArgumentStringOptional>("separator", 's', "Caracter separador de coordenadas. Por defecto ';'", &separator));
+  cmd.push_back(std::make_shared<ArgumentStringOptional>("coord_trf", 't', "Fichero de texto con las coordenadas transformadas", &coord_trf));
+  
+  cmd.addExample(cmd_name + " --epsg_in EPSG:25830 --epsg_out EPSG:4258 --coord 281815.044;4827675.243;123.35");
+  cmd.addExample(cmd_name + " -iEPSG:25830 -oEPSG:4258 --coord utm.txt");
 
-  cmd.addExample(cmd_name + " --epsg_in EPSG:25830 --epsg_out EPSG:4258 -x 281815.044 -y 4827675.243");
-  cmd.addExample(cmd_name + " -iEPSG:25830 -oEPSG:4258 -x281815.044 -y4827675.243");
 
-  // Parseo de los argumentos y comprobación de los mismos
   Command::Status status = cmd.parse(argc, argv);
   if (status == Command::Status::parse_error ) {
     return 1;
@@ -62,21 +61,58 @@ int main(int argc, char** argv)
   console.setLogLevel(MessageLevel::msg_verbose);
   MessageManager::instance().addListener(&console);
 
-  Point3D pt_utm(coord_x, coord_y, coord_z);
-  msgInfo("Coordenadas en %s: (%f,%f,%f)", epsg_in.c_str(), pt_utm.x, pt_utm.y, pt_utm.z);
-
   try {
+
     std::shared_ptr<Crs> epsgIn(new Crs(epsg_in));
     std::shared_ptr<Crs> epsgOut(new Crs(epsg_out));
     CrsTransform<Point3D> crs(epsgIn, epsgOut);
     
-    Point3D pt_geo;
-    crs.transform(pt_utm, &pt_geo);
+    std::ofstream ofs;
+    ofs.open(coord_trf, std::ofstream::out | std::ofstream::trunc);
 
-    msgInfo("Coordenadas transformadas en %s: (%f,%f,%f)", epsg_out.c_str(), pt_geo.x, pt_geo.y, pt_geo.z);
+    if (fs::exists(coord)) {
+    
+      std::ifstream ifs;
+      ifs.open(coord, std::ifstream::in);
+      if (ifs.is_open()) {
+
+        std::string line;
+        while (std::getline(ifs, line)) {
+          std::vector<double> vector;
+          splitToNumbers(line, vector, separator.c_str());
+          Point3D pt_in(vector[0], vector[1], vector[2]);
+          Point3D pt_out;
+          crs.transform(pt_in, &pt_out);
+          msgInfo("%lf;%lf;%lf -> %lf;%lf;%lf", vector[0], vector[1], vector[2], pt_out.x, pt_out.y, pt_out.z);
+
+          if (ofs.is_open()) {
+            ofs << vector[0] << separator << vector[1] << separator << vector[2] << separator << " -> "
+                << pt_out.x << separator << pt_out.y << separator << pt_out.z << std::endl;
+          }
+        }
+        ifs.close();
+      }
+
+    } else {
+      std::vector<double> point;
+      splitToNumbers(coord, point, separator.c_str());
+      Point3D pt_in(point[0], point[1], point[2]);
+      Point3D pt_out;
+      crs.transform(pt_in, &pt_out);
+      msgInfo("%lf;%lf;%lf -> %lf;%lf;%lf", point[0], point[1], point[2], pt_out.x, pt_out.y, pt_out.z);
+
+      if (ofs.is_open()) {
+        ofs << point[0] << separator << point[1] << separator << point[2] << separator << " -> "
+            << pt_out.x << separator << pt_out.y << separator << pt_out.z << std::endl;
+      }
+
+    }
+
+    if (ofs.is_open()) ofs.close();
+
   } catch (const std::exception &e) {
-
-  }
+    msgError(e.what());
+  } 
 
   return 0;
 }
