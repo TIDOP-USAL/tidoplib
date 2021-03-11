e5b99f8115259ea01960b6a8bc4a0d8b9762ac3a
estebanrdo <estebanrdo@gmail.com>
2021-03-11 17:37:32 +0100

    Nota de licencia modificada a LGPL-3.

--- src/tidop/experimental/designpatterns.h
+++ src/tidop/experimental/designpatterns.h
@@ -1,3 +1,27 @@
+/**************************************************************************
+ *                                                                        *
+ * Copyright (C) 2021 by Tidop Research Group                             *
+ * Copyright (C) 2021 by Esteban Ruiz de Oña Crespo                       *
+ *                                                                        *
+ * This file is part of TidopLib                                          *
+ *                                                                        *
+ * TidopLib is free software: you can redistribute it and/or modify       *
+ * it under the terms of the GNU Lesser General Public License as         *
+ * published by the Free Software Foundation, either version 3 of the     *
+ * License, or (at your option) any later version.                        *
+ *                                                                        *
+ * TidopLib is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ * GNU Lesser General Public License for more details.                    *
+ *                                                                        *
+ * You should have received a copy of the GNU Lesser General Public       *
+ * License along with Foobar. If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                        *
+ * @license LGPL-3.0 <https://www.gnu.org/licenses/lgpl-3.0.html>         *
+ *                                                                        *
+ **************************************************************************/
+ 
 #ifndef I3D_DESING_PATTERNS_H
 #define I3D_DESING_PATTERNS_H
 
--- src/tidop/featmatch/features.h
+++ src/tidop/featmatch/features.h
@@ -1,25 +1,26 @@
-/************************************************************************
- *                                                                      *
- * Copyright (C) 2020 by Tidop Research Group                           *
- *                                                                      *
- * This file is part of TidopLib                                        *
- *                                                                      *
- * TidopLib is free software: you can redistribute it and/or modify     *
- * it under the terms of the GNU General Public License as published by *
- * the Free Software Foundation, either version 3 of the License, or    *
- * (at your option) any later version.                                  *
- *                                                                      *
- * TidopLib is distributed in the hope that it will be useful,          *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
- * GNU General Public License for more details.                         *
- *                                                                      *
- * You should have received a copy of the GNU General Public License    *
- * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.      *
- *                                                                      *
- * @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>                *
- *                                                                      *
- ************************************************************************/
+/**************************************************************************
+ *                                                                        *
+ * Copyright (C) 2021 by Tidop Research Group                             *
+ * Copyright (C) 2021 by Esteban Ruiz de Oña Crespo                       *
+ *                                                                        *
+ * This file is part of TidopLib                                          *
+ *                                                                        *
+ * TidopLib is free software: you can redistribute it and/or modify       *
+ * it under the terms of the GNU Lesser General Public License as         *
+ * published by the Free Software Foundation, either version 3 of the     *
+ * License, or (at your option) any later version.                        *
+ *                                                                        *
+ * TidopLib is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ * GNU Lesser General Public License for more details.                    *
+ *                                                                        *
+ * You should have received a copy of the GNU Lesser General Public       *
+ * License along with Foobar. If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                        *
+ * @license LGPL-3.0 <https://www.gnu.org/licenses/lgpl-3.0.html>         *
+ *                                                                        *
+ **************************************************************************/
 
 #ifndef TL_FEATMATCH_FEATURES_H
 #define TL_FEATMATCH_FEATURES_H
