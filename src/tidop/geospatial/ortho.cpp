72c5e867c9907c0314aa7a520fcc2c67ea044f02
esteban <estebanrdo@gmail.com>
2022-01-01 18:10:26 +0100

    license: License link updated

--- src/tidop/geospatial/ortho.cpp
+++ src/tidop/geospatial/ortho.cpp
@@ -16,13 +16,13 @@
  * GNU Lesser General Public License for more details.                    *
  *                                                                        *
  * You should have received a copy of the GNU Lesser General Public       *
- * License along with Foobar. If not, see <http://www.gnu.org/licenses/>. *
+ * License along with TidopLib. If not, see <http://www.gnu.org/licenses>.*
  *                                                                        *
  * @license LGPL-3.0 <https://www.gnu.org/licenses/lgpl-3.0.html>         *
  *                                                                        *
  **************************************************************************/
 
-#include "ortho.h"
+#include "tidop/geospatial/ortho.h"
 
 #include "tidop/core/chrono.h"
 #include "tidop/core/progress.h"
--- src/tidop/geospatial/util.cpp
+++ src/tidop/geospatial/util.cpp
@@ -16,14 +16,14 @@
  * GNU Lesser General Public License for more details.                    *
  *                                                                        *
  * You should have received a copy of the GNU Lesser General Public       *
- * License along with Foobar. If not, see <http://www.gnu.org/licenses/>. *
+ * License along with TidopLib. If not, see <http://www.gnu.org/licenses>.*
  *                                                                        *
  * @license LGPL-3.0 <https://www.gnu.org/licenses/lgpl-3.0.html>         *
  *                                                                        *
  **************************************************************************/
 
-#include "crs.h"
-#include "util.h"
+#include "tidop/geospatial/crs.h"
+#include "tidop/geospatial/util.h"
 
 namespace tl
 {
