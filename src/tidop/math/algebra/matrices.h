bfadde6c4ffeb41acc510bee56a9b9d5aa2db5fe
Alberto Morcillo Sanz <amorcillosanz@gmail.com>
2023-07-19 13:38:40 +0200

    Matrices class

--- src/tidop/math/algebra/matrices.h
+++ src/tidop/math/algebra/matrices.h
@@ -5,101 +5,106 @@
 namespace tl
 {
 
-static Matrix4x4f scale(float sx, float sy, float sz)
+class Matrices 
 {
-    return Matrix4x4f
+
+    static Matrix4x4f scale(float sx, float sy, float sz)
     {
-        sx, 0.0f, 0.0f, 0.0f,
-        0.0f,  sy, 0.0f, 0.0f,
-        0.0f, 0.0f,  sz, 0.0f,
-        0.0f, 0.0f, 0.0f, 1.0f
-    };
-}
+        return Matrix4x4f
+        {
+            sx, 0.0f, 0.0f, 0.0f,
+            0.0f,  sy, 0.0f, 0.0f,
+            0.0f, 0.0f,  sz, 0.0f,
+            0.0f, 0.0f, 0.0f, 1.0f
+        };
+    }
 
-static Matrix4x4f translate(float tx, float ty, float tz)
-{
-    return Matrix4x4f
+    static Matrix4x4f translate(float tx, float ty, float tz)
     {
-        1.0f, 0.0f, 0.0f,  tx,
-        0.0f, 1.0f, 0.0f,  ty,
-        0.0f, 0.0f, 1.0f,  tz,
-        0.0f, 0.0f, 0.0f, 1.0f
-    };
-}
+        return Matrix4x4f
+        {
+            1.0f, 0.0f, 0.0f,  tx,
+            0.0f, 1.0f, 0.0f,  ty,
+            0.0f, 0.0f, 1.0f,  tz,
+            0.0f, 0.0f, 0.0f, 1.0f
+        };
+    }
 
-static Matrix4x4f rotationX(float angle)
-{
-    return Matrix4x4f
+    static Matrix4x4f rotationX(float angle)
     {
-        1.0f, 0.0f, 0.0f, 0.0f,
-        0.0f, cos(angle), -sin(angle), 0.0f,
-        0.0f, sin(angle), cos(angle), 0.0f,
-        0.0f, 0.0f, 0.0f, 1.0f
-    };
-}
+        return Matrix4x4f
+        {
+            1.0f, 0.0f, 0.0f, 0.0f,
+            0.0f, cos(angle), -sin(angle), 0.0f,
+            0.0f, sin(angle), cos(angle), 0.0f,
+            0.0f, 0.0f, 0.0f, 1.0f
+        };
+    }
 
-static Matrix4x4f rotationY(float angle)
-{
-    return Matrix4x4f
+    static Matrix4x4f rotationY(float angle)
     {
-        cos(angle), 0.0f, sin(angle), 0.0f,
-        0.0f, 1.0f, 0.0f, 0.0f,
-        -sin(angle), 0.0f, cos(angle), 0.0f,
-        0.0f, 0.0f, 0.0f, 1.0f
-    };
-}
+        return Matrix4x4f
+        {
+            cos(angle), 0.0f, sin(angle), 0.0f,
+            0.0f, 1.0f, 0.0f, 0.0f,
+            -sin(angle), 0.0f, cos(angle), 0.0f,
+            0.0f, 0.0f, 0.0f, 1.0f
+        };
+    }
 
-static Matrix4x4f rotationZ(float angle)
-{
-    return Matrix4x4f
+    static Matrix4x4f rotationZ(float angle)
     {
-        cos(angle), sin(angle), 0.0f, 0.0f,
-        -sin(angle), cos(angle), 0.0f, 0.0f,
-        0.0f, 0.0f, 1.0f, 0.0f,
-        0.0f, 0.0f, 0.0f, 1.0f
-    };
-}
+        return Matrix4x4f
+        {
+            cos(angle), sin(angle), 0.0f, 0.0f,
+            -sin(angle), cos(angle), 0.0f, 0.0f,
+            0.0f, 0.0f, 1.0f, 0.0f,
+            0.0f, 0.0f, 0.0f, 1.0f
+        };
+    }
 
-static Matrix4x4f perspective(float fovy, float aspect, float nearZ, float farZ)
-{
-    float m11 = 1 / (aspect * tan(fovy / 2));
-    float m22 = 1 / tan(fovy / 2);
-    float m33 = (-nearZ - farZ) / (nearZ - farZ);
-    float m34 = (2 * nearZ * farZ) / (nearZ - farZ);
-
-    return Matrix4x4f
+    static Matrix4x4f perspective(float fovy, float aspect, float nearZ, float farZ)
     {
-        m11 , 0.0f, 0.0f, 0.0f,
-        0.0f,  m22, 0.0f, 0.0f,
-        0.0f, 0.0f,  m33,  m34,
-        0.0f, 0.0f,  1.0f, 0.0f
-    };
-}
+        float m11 = 1 / (aspect * tan(fovy / 2));
+        float m22 = 1 / tan(fovy / 2);
+        float m33 = (-nearZ - farZ) / (nearZ - farZ);
+        float m34 = (2 * nearZ * farZ) / (nearZ - farZ);
 
-static Matrix4x4f lookAt(const Vector3f& position, const Vector3f& target, const Vector3f& up) {
+        return Matrix4x4f
+        {
+            m11 , 0.0f, 0.0f, 0.0f,
+            0.0f,  m22, 0.0f, 0.0f,
+            0.0f, 0.0f,  m33,  m34,
+            0.0f, 0.0f,  1.0f, 0.0f
+        };
+    }
 
-    auto dot = [&](const Vector3f& u, const Vector3f& v) {
-        return u.x() * v.x() + u.y() * v.y() + u.z() * v.z();
-    };
+    static Matrix4x4f lookAt(const Vector3f& position, const Vector3f& target, const Vector3f& up) {
 
-    auto cross = [&](const Vector3f& u, const Vector3f& v) {
-        return Vector3f{ u.y() * v.z() - u.z() * v.y(), -u.x() * v.z() + u.z() * v.x(), u.x() * v.y() - u.y() * v.x() };
-    };
+        auto dot = [&](const Vector3f& u, const Vector3f& v) {
+            return u.x() * v.x() + u.y() * v.y() + u.z() * v.z();
+        };
 
-    Vector3f zaxis = { position.x() - target.x(), position.y() - target.y(), position.z() - target.z() };
-    zaxis.normalize();
+        auto cross = [&](const Vector3f& u, const Vector3f& v) {
+            return Vector3f{ u.y() * v.z() - u.z() * v.y(), -u.x() * v.z() + u.z() * v.x(), u.x() * v.y() - u.y() * v.x() };
+        };
 
-    Vector3f xaxis = cross(up, zaxis);
-    xaxis.normalize();
+        Vector3f zaxis = { position.x() - target.x(), position.y() - target.y(), position.z() - target.z() };
+        zaxis.normalize();
 
-    Vector3f yaxis = cross(zaxis, xaxis);
+        Vector3f xaxis = cross(up, zaxis);
+        xaxis.normalize();
 
-    return Matrix4x4f{
-        xaxis.x(), xaxis.y(), xaxis.z(), -dot(xaxis, position),
-        yaxis.x(), yaxis.y(), yaxis.z(), -dot(yaxis, position),
-        zaxis.x(), zaxis.y(), zaxis.z(), -dot(zaxis, position),
-        0, 0, 0, 1
-    };
-}
+        Vector3f yaxis = cross(zaxis, xaxis);
+
+        return Matrix4x4f{
+            xaxis.x(), xaxis.y(), xaxis.z(), -dot(xaxis, position),
+            yaxis.x(), yaxis.y(), yaxis.z(), -dot(yaxis, position),
+            zaxis.x(), zaxis.y(), zaxis.z(), -dot(zaxis, position),
+            0, 0, 0, 1
+        };
+    }
+
+};
 
 }
